8083546c5e72758cc7a43190155a512f6a1710f0
Yves Talboom <yves.talboom@gmail.com>
2016-03-11 07:46:01 +0100

    Add timestamps false to plant history

--- app/PlantHistory.php
+++ app/PlantHistory.php
@@ -6,6 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class PlantHistory extends Model
 {
+    public $timestamps = false;
+
     public function plant()
     {
         return $this->belongsTo(Plant::class);
