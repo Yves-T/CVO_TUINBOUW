560f6d9a0bbc488368981e25722f532d3cb78ba1
Yves Talboom <yves.talboom@gmail.com>
2016-03-15 08:30:55 +0100

    Use relative urls in data service

--- public/scripts/service/dataService.js
+++ public/scripts/service/dataService.js
@@ -11,23 +11,23 @@ angular
         };
 
         var getPlants = function (success, error) {
-            $http.get('http://tuinbouw.app/api/plant').success(success).error(error);
+            $http.get('api/plant').success(success).error(error);
         };
 
         var getPlantById = function (id, success, error) {
-            $http.get('http://tuinbouw.app/api/plant/' + id).success(success).error(error);
+            $http.get('api/plant/' + id).success(success).error(error);
         };
 
         var updatePlant = function (id, formData, success, error) {
-            $http.put('http://tuinbouw.app/api/plant/' + id, formData).success(success).error(error);
+            $http.put('api/plant/' + id, formData).success(success).error(error);
         };
 
         var deletePlant = function (plantId, success, error) {
-            $http.delete('http://tuinbouw.app/api/plant/' + plantId).success(success).error(error);
+            $http.delete('api/plant/' + plantId).success(success).error(error);
         };
 
         var createPlant = function (formData, success, error) {
-            $http.post('http://tuinbouw.app/api/plant', formData).success(success).error(error);
+            $http.post('api/plant', formData).success(success).error(error);
         };
 
         return {
